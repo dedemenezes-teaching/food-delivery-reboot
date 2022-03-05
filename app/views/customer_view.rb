c9e579034229f5eefe4ef0ffe7ae4ec76f53b577
Andre Menezes <djdedemenezes@gmail.com>
2022-03-05 15:38:30 -0300

    base view as another example of inheritance

--- app/views/customer_view.rb
+++ app/views/customer_view.rb
@@ -1,9 +1,6 @@
-class CustomerView
-  def ask_for(item)
-    puts "#{item.capitalize}?"
-    gets.chomp
-  end
+require_relative 'base_view'
 
+class CustomerView < BaseView
   def display(customers)
     customers.each_with_index do |customer, index|
       puts "#{index + 1} - #{customer.name} - #{customer.address}"
--- app/views/meal_view.rb
+++ app/views/meal_view.rb
@@ -1,9 +1,6 @@
-class MealView
-  def ask_for(item)
-    puts "#{item.capitalize}?"
-    gets.chomp
-  end
+require_relative 'base_view'
 
+class MealView < BaseView
   def display(meals)
     meals.each_with_index do |meal, index|
       puts "#{index + 1} - #{meal.name} - #{meal.price}"
